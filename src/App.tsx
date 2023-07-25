1512f343bd0ca756ae8ead45ca371cd459b3e8c3
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2023-07-25 21:11:52 +0900

    ルーティング機能の確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import { Tweets } from "./components/Tweets";
 import { Users } from "./components/Users";
+import { Login } from "./components/login";
+import { SignUp } from "./components/SignUp";
 
 const App = () => (
   <>
-    <h1>Hello, World!</h1>
-    <Users />
-    <Tweets />
+    <Routes>
+      <Route path="login" element={<Login />}></Route>
+      <Route path="signup" element={<SignUp />}></Route>
+      <Route path="users" element={<Users />}></Route>
+      <Route path="tweets" element={<Tweets />}></Route>
+    </Routes>
+    <ul>
+      <li>
+        <NavLink
+          style={({ isActive }) => isActive ? { color: 'blue' } : undefined }
+          to="login"
+        >
+          login
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive }) => isActive ? { color: 'blue' } : undefined }
+          to="signup"
+        >
+          signup
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive }) => isActive ? { color: 'blue' } : undefined }
+          to="users"
+        >
+          users
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          style={({ isActive }) => isActive ? { color: 'blue' } : undefined }
+          to="tweets">
+          tweets
+        </NavLink>
+      </li>
+    </ul>
   </>
   
 )
