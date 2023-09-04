64a52e61caf6dc8be19f0714571487bafc95e3f5
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2023-09-04 21:48:37 +0900

    ログアウト機能と認証状況による表示の切り替え実装

--- src/components/common/menu-list-component.tsx
+++ src/components/common/menu-list-component.tsx
@@ -4,62 +4,150 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Tooltip,
 } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import ForumIcon from '@mui/icons-material/Forum';
 import PeopleIcon from '@mui/icons-material/People';
-import { NavLink, useLocation } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  AuthCommunicationService
+} from '../../services/auth-communication-service';
+import { ErrorResponse } from '../../models/shared-params';
+import { useAuthContext } from '../../services/auth-context-service';
+import {
+  ResponseState,
+  useResponseContext
+} from '../../services/response-context-service';
+
+/**
+ * メニューに表示する情報の定義
+ */
+interface MenuItemInfo {
+  /** 遷移パス */
+  path: string;
+  /** Icon Element */
+  element: JSX.Element;
+  /** ログインユーザ画面に表示？ */
+  isPrivate: boolean;
+  /** ハンドラ */
+  handler: (path: string) => void;
+}
 
 /**
  * メニューリスト
  */
 export const MenuListComponent = (props: { open: boolean }) => {
   const { open } = props;
-  const menuList = [
-    ['login', <LoginIcon />],
-    ['signup', <PersonAddIcon />],
-    ['tweets', <ForumIcon />],
-    ['users', <PeopleIcon />],
-  ];
+
   const location = useLocation();
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuthContext();
+  const { setResponse } = useResponseContext();
+
+  /**
+   * 通常ハンドラ
+   * @param path 遷移パス
+   */
+  const handleClick = (path: string) => {
+    navigate(`/${path}`);
+  }
+
+  /**
+   * ログアウト用ハンドラ
+   */
+  const handleLogoutClick = () => {
+    const authCommunicationService = new AuthCommunicationService();
+    authCommunicationService.destroy()
+    .then(() => {
+      setResponse(new ResponseState());
+      setAuth(null);
+      navigate('/login');
+    })
+    .catch(e => {
+      if (e !== undefined) {
+        const { message } = e.data as ErrorResponse;
+        const responseState = new ResponseState();
+        responseState.severity = 'error';
+        responseState.message = message;
+        setResponse(responseState);
+      }
+    });
+  }
+
+  /**
+   * メニューに表示する情報リスト
+   */
+  const menuList: MenuItemInfo[] = [{
+    path: 'login',
+    element: <LoginIcon />,
+    isPrivate: false,
+    handler: handleClick,
+  }, {
+    path: 'signup',
+    element: <PersonAddIcon />,
+    isPrivate: false,
+    handler: handleClick,
+  }, {
+    path: 'tweets',
+    element: <ForumIcon />,
+    isPrivate: true,
+    handler: handleClick,
+  }, {
+    path: 'users',
+    element: <PeopleIcon />,
+    isPrivate: true,
+    handler: handleClick,
+  }, {
+    path: 'logout',
+    element: <LogoutIcon />,
+    isPrivate: true,
+    handler: handleLogoutClick,
+  }];
 
   return (
     <List>
-      {menuList.map(([text, icon], i) => (
-        <ListItem
-          key={i}
-          disablePadding
-          sx={{ display: 'block' }}
+      {menuList
+      .filter(({ isPrivate }) => !!auth === isPrivate)
+      .map(({ path, element, handler }) => (
+        <Tooltip
+          title={path}
+          placement='right-end'
+          key={path}
         >
-          <ListItemButton
-            sx={{
-              minHeight: 48,
-              justifyContent: open ? 'initial' : 'center',
-              px: 2.5,
-            }}
-            selected={location.pathname===`/${text}`}
-            component={NavLink}
-            to={`/${text}`}
+          <ListItem
+            disablePadding
+            sx={{ display: 'block' }}
           >
-            <ListItemIcon
+            <ListItemButton
+              onClick={() => handler(path)}
               sx={{
-                minWidth: 0,
-                mr: open ? 3 : 'auto',
-                justifyContent: 'center',
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
               }}
+              selected={location.pathname === `/${path}`}
             >
-              {icon}
-            </ListItemIcon>
-            
-            <ListItemText
-              primary={text}
-              sx={{ opacity: open ? 1 : 0 }}
-            />
-          </ListItemButton>
-        </ListItem>
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {element}
+              </ListItemIcon>
+              
+              <ListItemText
+                primary={path}
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+            </ListItemButton>
+          </ListItem>
+        </Tooltip>
       ))}
     </List>
   )
 }
-
